721337246660ca628a495cac61eb4ea9a1fd1d30
Lloyd Najac <lloyd.najac@stu.ocsb.ca>
2022-09-27 15:37:54 +0000

    Added area_per_rect_input.py

---
+++ area_per_rect_input.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Created By: Lloyd Najac
+# Date: Sep. 27, 2022
+# This program calculates and displays the area and perimeter for a given user input.
+
+
+def main():
+    print()
+
+
+if __name__ == "__main__":
+    main()
